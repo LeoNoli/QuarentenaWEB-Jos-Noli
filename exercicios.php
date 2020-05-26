9003c504e198502d599256556306ee8f49441291
LeoNoli <nollizleo@gmail.com>
2020-05-25 23:04:39 -0300

    Quarentena

--- exercicios.php
+++ exercicios.php
@@ -1,34 +1,37 @@
-<!DOCTYPE html>
-<html>
-
+<!DOCTYPE HTML>
+<html lang="pt-br">
     <head>
-        <meta charset="UTF-8">
-        <link href="bootstrap-4.4.1-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
-        <title>Title</title>
-        <script>
-            function soma(vl1, vl2){
-                vl1=parseInt(vl1);
-                valor2=parseInt(vl2);
-                var resultado= vl1+vl2;
-                if(!isNaN(vl1) && !isNaN(vl2) ){
-                    document.calculadora.resultado.value=resultado;
-                }
-            }
-            function limpa(){
-                document.calculadora.numero1.value="";
-                document.calculadora.numero2.value="";
-                document.calculadora.resultado.value="";
-            }
-        </script>
+        <meta charset="UTF-8"/>
+        <title>Exercício Quarentena</title>
     </head>
     <body>
-        <form name="calculadora">
-            <input type="number" name="numero1" placeholder="Operando 1"> +
-            <input type="number" name="numero2" placeholder="Operando 2"> =
-            <input type="number" name="resultado" placeholder="resultado..." readonly=“true”>
-            <br><br>
-            <input type="button" value="Calcular" onclick="soma(document.calculadora.numero1.value, document.calculadora.numero2.value);">
-            <input type="button" value="Limpar" onclick="limpa();">
-        </form>
+        <?php
+            if(empty($_POST)){
+                echo'<form action="exercicios.php" method="POST">
+                    <input type="text" name="num1" placeholder="Digite um valor..."/> 
+                    +
+                    <input type="text" name="num2" placeholder="Digite um valor..."/> 
+                    =	
+		            <input type="text" name="resp" value=" " placeholder="Resposta..."/>
+				
+		            <input type="submit" value="Somar" />
+		            <input type="reset" value="Limpar" />
+                </form>';
+            }
+            else{
+                $n1=$_POST["n1"];
+                $n2=$_POST["n2"];
+                $resp=$n1+$n2;
+
+                echo '<form action="exequarentena.php" method="POST">
+                    <input type="text" name="num1" value="'.$num1.'" placeholder="Digite um valor..."/> +
+                    <input type="text" name="num2" value="'.$num2.'" placeholder="Digite um valor..."/> =	
+                    <input type="text" name="resp" value="'.$resp.'"placeholder="Resposta..."/>
+                        
+                    <input type="submit" value="Somar" />
+                    <input type="reset" value="Apagar" />
+                </form>';
+                }
+        ?>
     </body>
 </html>
